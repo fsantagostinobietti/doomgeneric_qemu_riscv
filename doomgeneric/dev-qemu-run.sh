bb6edad8d8855f09775dfb99004a387ea43bfb5a
fsantagostinobietti <6057026+fsantagostinobietti@users.noreply.github.com>
2025-10-30 21:09:39 +0100

    dev script

---
+++ doomgeneric/dev-qemu-run.sh
@@ -0,0 +1,21 @@
+
+# Detect OS and pick default executable if QEMU is not set
+: "${QEMU:=""}"
+if [ -z "$QEMU" ]; then
+    uname_s="$(uname -s 2>/dev/null || echo Unknown)"
+    case "$uname_s" in
+        Darwin*) QEMU="$HOME/StudioProjects/qemu/build/qemu-system-riscv64-unsigned"
+                 AUDIO="sdl"  # coreaudio not working
+                 ;;
+        Linux*)  QEMU="$HOME/programs/qemu-10.1.0/build/qemu-system-riscv64"
+                 AUDIO="pa"  #  oss not working, alsa has noise
+                 ;;
+        *)       QEMU="qemu-system-riscv64"
+                 AUDIO="default"
+                 ;; # conservative default
+    esac
+fi
+
+QEMU="$QEMU" AUDIO="$AUDIO" bash qemu-run.sh
+
+
