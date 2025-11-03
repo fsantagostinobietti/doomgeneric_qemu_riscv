2adb51c4ecc3fd24f13b6384f929bb8cbf7f80a9
fsantagostinobietti <6057026+fsantagostinobietti@users.noreply.github.com>
2025-11-03 17:03:00 +0100

    start scripts update

--- doomgeneric/dev-qemu-run.sh
+++ doomgeneric/dev-qemu-run.sh
@@ -5,17 +5,17 @@ if [ -z "$QEMU" ]; then
     uname_s="$(uname -s 2>/dev/null || echo Unknown)"
     case "$uname_s" in
         Darwin*) QEMU="$HOME/StudioProjects/qemu-10.1.2/build/qemu-system-riscv64-unsigned"
-                 AUDIO="coreaudio"
+                 AUDIO_DEVICE="-device virtio-sound-device,id=vsnd"
                  ;;
         Linux*)  QEMU="$HOME/programs/qemu-10.1.0/build/qemu-system-riscv64"
-                 AUDIO="pa"  #  oss not working, alsa has noise
+                 AUDIO_DEVICE="-audiodev pa,id=snd0 -device virtio-sound-device,audiodev=snd0,id=vsnd" #  oss not working, alsa has noise
                  ;;
         *)       QEMU="qemu-system-riscv64"
-                 AUDIO="default"
+                 AUDIO_DEVICE="-device virtio-sound-device,id=vsnd"
                  ;; # conservative default
     esac
 fi
 
-QEMU="$QEMU" AUDIO="$AUDIO" bash qemu-run.sh
+QEMU="$QEMU" AUDIO_DEVICE="$AUDIO_DEVICE" bash qemu-run.sh
 
 
--- doomgeneric/qemu-run.sh
+++ doomgeneric/qemu-run.sh
@@ -1,14 +1,13 @@
 
 
 : "${QEMU:="qemu-system-riscv64"}"
-: "${AUDIO:="default"}"
+: "${AUDIO_DEVICE:="-device virtio-sound-device,id=vsnd"}"
 
 # -global virtio-mmio.force-legacy=false : disable legacy virtio-mmio (version 1)
 # -device virtio-keyboard-device,id=vkbd : virtualized keyboard
 # -audiodev $AUDIO,id=snd0 -device virtio-sound-device,audiodev=snd0,id=vsnd : virtualized audio
 "$QEMU" -global virtio-mmio.force-legacy=false -machine virt -m 128M \
- -device virtio-keyboard-device,id=vkbd \
- -audiodev $AUDIO,id=snd0 -device virtio-sound-device,audiodev=snd0,id=vsnd \
+ -device virtio-keyboard-device,id=vkbd $AUDIO_DEVICE \
  -device ramfb \
  -bios none -serial stdio \
  -kernel doomgeneric \
